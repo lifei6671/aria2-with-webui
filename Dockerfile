089e6073773ece91b85e2723f9202b62ec277817
lifeilin01 <lifeilin01@58.com>
2020-03-16 19:16:49 +0800

    .

--- Dockerfile
+++ Dockerfile
@@ -1,30 +1,14 @@
-FROM alpine:edge
+FROM alpine:3.4
 
-MAINTAINER xujinkai <jack777@xujinkai.net>
+VOLUME /data
 
-RUN apk update && \
-	apk add --no-cache --update bash && \
-	mkdir -p /conf && \
-	mkdir -p /conf-copy && \
-	mkdir -p /data && \
-	apk add --no-cache --update aria2 && \
-	apk add git && \
-	git clone https://github.com/ziahamza/webui-aria2 /aria2-webui && \
-    rm /aria2-webui/.git* -rf && \
-    apk del git && \
-	apk add --update darkhttpd
-
-ADD files/start.sh /conf-copy/start.sh
-ADD files/aria2.conf /conf-copy/aria2.conf
-ADD files/on-complete.sh /conf-copy/on-complete.sh
-
-RUN chmod +x /conf-copy/start.sh
-
-WORKDIR /
-VOLUME ["/data"]
-VOLUME ["/conf"]
 EXPOSE 6800
-EXPOSE 80
-EXPOSE 8080
 
-CMD ["/conf-copy/start.sh"]
+ADD files/ /conf-copy/
+ADD files/start.sh /conf/
+RUN chmod +x /conf/start.sh
+
+ENTRYPOINT [ "/conf/start.sh" ]
+CMD aria2c --conf-path=/conf/aria2.conf
+
+RUN apk add --update --no-cache aria2 && rm -rf /var/cache/apk/*
--- files/start.sh
+++ files/start.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
+
 if [ ! -f /conf/aria2.conf ]; then
-	cp /conf-copy/aria2.conf /conf/aria2.conf
+  if [ -f /data/aria2.conf ];then
+    cp /data/aria2.conf /conf/aria2.conf
+  else
+	  cp /conf-copy/aria2.conf /conf/aria2.conf
+	fi
 	if [ $SECRET ]; then
 		echo "rpc-secret=${SECRET}" >> /conf/aria2.conf
+		echo "rpc-secure=true" >> /conf/aria2.conf
 	fi
 fi
 if [ ! -f /conf/on-complete.sh ]; then
@@ -10,8 +16,33 @@ if [ ! -f /conf/on-complete.sh ]; then
 fi
 
 chmod +x /conf/on-complete.sh
-touch /conf/aria2.session
+if [ ! -d /data/aria2 ];then
+  mkdir /data/aria2
+fi
 
-darkhttpd /aria2-webui/docs --port 80 &
-darkhttpd /data --port 8080 &
-aria2c --conf-path=/conf/aria2.conf
+if [ ! -f /data/aria2/session.dat ];then
+  touch /data/aria2/session.dat
+fi
+
+if [ ! -f /data/aria2/aria2.log ];then
+touch /data/aria2/aria2.log
+
+fi
+if [ ! -f /data/aria2/session.dat ];then
+  touch /data/aria2/session.dat
+fi
+
+if [ ! -f /data/aria2/cookies ];then
+  touch /data/aria2/cookies
+fi
+
+if [ ! -f /data/aria2/dht.dat ];then
+  touch /data/aria2/dht.dat
+fi
+
+if [ ! -f /data/aria2/dht6.dat ];then
+  touch /data/aria2/dht6.dat
+fi
+
+exec "$@"
+
